f5e880fad837056f96b1ad3e11b44235ef5b5d3e
angulodev <angulodev@users.noreply.github.com>
2026-07-05 00:09:21 +0000

    Hábitos: sección Archivados con restaurar y eliminar definitivo (+limpieza de logs)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'atomico-v1';
+const CACHE = 'atomico-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
